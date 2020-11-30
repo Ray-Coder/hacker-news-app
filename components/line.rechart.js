3569479ae81951c0778399d471cee57dc24d2e62
Payel Roy Barman <payelroybarman@Payels-MacBook-Pro.local>
2020-12-01 02:50:02 +0530

    pushing image

--- components/line.rechart.js
+++ components/line.rechart.js
@@ -6,7 +6,7 @@ class LineRechartComponent extends React.Component {
 
     render() {
         return (
-            <LineChart width={550} height={250} data={this.props.plot}
+            <LineChart width={700} height={300} data={this.props.plot}
                 margin={{ top: 40, right: 0, left: 0, bottom: 5 }}>
                 <CartesianGrid strokeDasharray="3 3" />
                 <XAxis dataKey="story" />
