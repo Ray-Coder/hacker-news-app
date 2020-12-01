f25133ef862ce465ba23b538ec4255b41965f206
Payel Roy Barman <payelroybarman@Payels-MacBook-Pro.local>
2020-12-01 10:32:51 +0530

    making chart responsive

--- components/line.rechart.js
+++ components/line.rechart.js
@@ -1,12 +1,13 @@
 import React from "react";
-import { LineChart, Line, YAxis, XAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+import { LineChart, Line, YAxis, XAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
 class LineRechartComponent extends React.Component {
    
 
     render() {
         return (
-            <LineChart width={700} height={300} data={this.props.plot}
+            <ResponsiveContainer width="95%" height={400}>
+            <LineChart  data={this.props.plot}
                 margin={{ top: 40, right: 0, left: 0, bottom: 5 }}>
                 <CartesianGrid strokeDasharray="3 3" />
                 <XAxis dataKey="story" />
@@ -15,6 +16,8 @@ class LineRechartComponent extends React.Component {
                 <Legend/>
                 <Line type="monotone" name="Story_Ids" dataKey="points" stroke="#0095FF" />
             </LineChart>
+            </ResponsiveContainer>
+            
         )
     };
 }
